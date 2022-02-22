8024341de618f35def9223f05575c21edd5373ea
andreygrigorev <grigorevandreyzool@gmail.com>
2022-02-22 13:05:02 +0400

    used assert to auto test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,10 @@
 // @ts-check
-
+// @ts-ignore
+import { strict as assert } from 'assert';
 import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
